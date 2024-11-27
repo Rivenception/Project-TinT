14538e1662c17ae9f454465fab645b3559f081f9
Rivenception <lootz.dev@gmail.com>
2024-11-27 11:55:02 -0500

    added flavicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,8 +4,8 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - Studio',
-    default: 'Studio - Award winning developer studio based in Denmark',
+    template: '%s - Project: Time in Trenches',
+    default: 'Project: Time in Trenches',
   },
 }
 
